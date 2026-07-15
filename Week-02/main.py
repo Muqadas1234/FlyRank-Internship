a0a38da0fed9d6ae07239ed8f73c7217d701c4a1
Muqadas Solangi <muqadassolangi.bssedcf22@iba-suk.edu.pk>
2026-07-15 17:43:38 +0500

    Week 2 - Stage 4: Full CRUD

--- Week-02/main.py
+++ Week-02/main.py
@@ -29,6 +29,10 @@ class TaskCreate(BaseModel):
     title: str
 
 
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
 # Root endpoint
 @app.get("/")
 def home():
@@ -89,3 +93,30 @@ def create_task(task: TaskCreate):
 
     # Return created task
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
